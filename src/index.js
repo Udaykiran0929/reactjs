2db3e8f5fc219827508a671f9f4e7f72cc6eea1f
Udaykiran0929 <udaykirandasari@lyrostech.com>
2024-03-11 12:31:53 +0530

    App.COFFEE SHOP

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App18 from './App18';
 import App19 from './App19';
+import AppCoffee from './AppCoffee'
 // import './index.css';    
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
      {/* <App4 Students={{name:"john", age:23}}/> */}
-     <App19/>
+     <AppCoffee/>
   </React.StrictMode>
 );
 
